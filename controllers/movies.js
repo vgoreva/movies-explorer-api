6e74d7db9dd477cfec5bbc338ee350ba242bce20
Vlada Goreva <mail@vgoreva.ru>
2023-09-17 09:56:31 +0300

    Исправила запрос фильмов пользователя

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,7 +6,7 @@ const NotFoundError = require('../errors/NotFoundError');
 const ForbiddenError = require('../errors/ForbiddenError');
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
+  Movie.find({ owner: req.user._id })
     .then((cards) => res.status(HTTP_STATUS_OK).send(cards))
     .catch(next);
 };
